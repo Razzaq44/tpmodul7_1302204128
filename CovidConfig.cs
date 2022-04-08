0beb6940837e3a1b35c238c1d9522de6053f096d
razzaq <razzaq@student.telkomuniversity.ac.id>
2022-04-08 17:02:00 +0700

    menyelesaikan program dengan runtime configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TP7
+{
+	public class CovidConfig
+	{
+		public string satuan_suhu { get; set; }
+		public string batas_hari_deman { get; set; }
+		public string pesan_ditolak { get; set; }
+		public string pesan_diterima { get; set; }
+		public static void UbahSatuan()
+        {
+			string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+			string pathAndFile = path + "/covid_config.json";
+			string json = File.ReadAllText(pathAndFile);
+
+			var baca = System.Text.Json.JsonSerializer.Deserialize<CovidConfig>(json);
+
+			if (baca.satuan_suhu == "celcius")
+            {
+				JObject jObject = Newtonsoft.Json.JsonConvert.DeserializeObject(json) as JObject;
+				JToken jToken = jObject.SelectToken("satuan_suhu");
+				jToken.Replace("fahrenheit");
+				string updatedJsonString = jObject.ToString();
+				File.WriteAllText(pathAndFile, updatedJsonString);
+			} 
+			else if (baca.satuan_suhu == "fahrenheit")
+            {
+				JObject jObject = Newtonsoft.Json.JsonConvert.DeserializeObject(json) as JObject;
+				JToken jToken = jObject.SelectToken("satuan_suhu");
+				jToken.Replace("celcius");
+				string updatedJsonString = jObject.ToString();
+				File.WriteAllText(pathAndFile, updatedJsonString);
+			}
+            else
+            {
+				JObject jObject = Newtonsoft.Json.JsonConvert.DeserializeObject(json) as JObject;
+				JToken jToken = jObject.SelectToken("satuan_suhu");
+				jToken.Replace("celcius");
+				string updatedJsonString = jObject.ToString();
+				File.WriteAllText(pathAndFile, updatedJsonString);
+			}			
+		}
+
+		public static void setDefault()
+        {
+			string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+			string pathAndFile = path + "/covid_config.json";
+			string json = File.ReadAllText(pathAndFile);
+
+			JObject jObject = Newtonsoft.Json.JsonConvert.DeserializeObject(json) as JObject;
+			JToken jToken = jObject.SelectToken("satuan_suhu");
+			jToken.Replace("celcius");
+			string updatedJsonString = jObject.ToString();
+			File.WriteAllText(pathAndFile, updatedJsonString);
+		}
+		
+		public CovidConfig() { }
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace TP7
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            CovidConfig.setDefault();
+            string input = "Y";
+            while (input != "N")
+            {
+                CovidConfig objHasilBaca = ReadJSON();
+
+                Console.WriteLine("Berapa suhu badan anda saat ini ? Dalam nilai " + objHasilBaca.satuan_suhu + " ");
+                double suhu = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman? ");
+                int hari = Convert.ToInt32(Console.ReadLine());
+
+                if (objHasilBaca.satuan_suhu == "celcius" && suhu >= 36.5 && suhu <= 37.5 && hari < Convert.ToInt32(objHasilBaca.batas_hari_deman))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(objHasilBaca.pesan_diterima);
+                }
+                else if (objHasilBaca.satuan_suhu == "fahrenheit" && suhu < 97.7 && suhu > 99.5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(objHasilBaca.pesan_diterima);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(objHasilBaca.pesan_ditolak);
+                    Console.WriteLine();
+                }
+
+                
+                Console.WriteLine();
+                Console.WriteLine("Ubah Satuan Suhu? (Y/N)");
+                
+                input = Console.ReadLine();
+                if (input == "Y")
+                {
+                    CovidConfig.UbahSatuan();
+                    Console.WriteLine();
+                }
+                else if (input != "Y" || input != "N")
+                {
+                    Console.WriteLine("Inputan salah!");
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+            }             
+        }
+        public static CovidConfig ReadJSON()
+        {
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string pathAndFile = path + "/covid_config.json";
+            string json = File.ReadAllText(pathAndFile);
+
+            CovidConfig config = JsonSerializer.Deserialize<CovidConfig>(json);
+
+            return config;
+        }
+    }
+}
+
+
